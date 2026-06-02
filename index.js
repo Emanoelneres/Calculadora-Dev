2d3c725d06c77a56db1a59c8b0028de47ab7ac19
Messias Emanoel Neres Nogueira <messiasemanoel9933@gmail.com>
2026-06-01 22:20:35 -0300

    fix: correção na captura de valores dos botões e ajuste de IDs

---
+++ index.js
@@ -0,0 +1,24 @@
+const buttonDeleteOne = document.getElementById("button-ac");
+const buttonDeleteAll = document.getElementById("button-del");
+const buttonNumberPercent = document.getElementById("button-percent");
+const buttonNumberDivision = document.getElementById("button-division");
+const buttonNumberEight = document.getElementById("button-eight");
+const buttonNumberSeven = document.getElementById("button-seven");
+const buttonNumberMulti = document.getElementById("button-multi");
+const butonNumberFour = document.getElementById("button-four");
+const buttonNumberFive = document.getElementById("button-five");
+const buttonNumberSix = document.getElementById("button-six");
+const buttonNumberSubtract = document.getElementById("button-subtract");
+const buttonNumberOne = document.getElementById("button-one");
+const buttonNumberTwo = document.getElementById("button-two");
+const buttonNumberTree = document.getElementById("button-tree");
+const buttonNumberMore = document.getElementById("button-more");
+const buttonNumberZero = document.getElementById("button-zero");
+const buttonPoint = document.getElementById("point");
+const buttonEqual = document.getElementById("equal");//Mapeamento dos botões
+
+function insert(num){
+
+document.getElementById('input-calc').value += num;
+    
+}
